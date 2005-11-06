19b75d9ee49699058d1e85657b386074766edb78
fabfoot <fabfoot>
2005-11-06 14:30:37 +0000

    nouvel etat PreConditionCheckerState

---
+++ PAGOD/src/pagod/wizard/control/states/PreConditionCheckerState.java
@@ -0,0 +1,49 @@
+/*
+ * Projet PAGOD
+ * 
+ * $Id: PreConditionCheckerState.java,v 1.1 2005/11/06 14:30:37 fabfoot Exp $
+ */
+package pagod.wizard.control.states;
+
+import pagod.common.model.Activity;
+import pagod.wizard.control.ActivityScheduler;
+
+/**
+ * @author fabfoot
+ *
+ */
+public class PreConditionCheckerState extends ActivityState
+{
+
+    /**
+     * @param activityScheduler
+     * @param activity
+     */
+    public PreConditionCheckerState(ActivityScheduler activityScheduler, Activity activity)
+    {
+        super(activityScheduler, activity);
+        // TODO Corps de constructeur généré automatiquement
+    }
+
+    @Override
+    public void previous()
+    {
+        // TODO Corps de méthode généré automatiquement
+        
+    }
+
+    @Override
+    public void next()
+    {
+        // TODO Corps de méthode généré automatiquement
+        
+    }
+
+    @Override
+    public void terminate()
+    {
+        // TODO Corps de méthode généré automatiquement
+        
+    }
+
+}
