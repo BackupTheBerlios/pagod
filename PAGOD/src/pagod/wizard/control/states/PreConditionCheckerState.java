276f1a77ea13455e4b08a932099ba3f64deae50d
fabfoot <fabfoot>
2005-11-06 14:55:46 +0000

    *** empty log message ***

--- PAGOD/src/pagod/wizard/control/states/PreConditionCheckerState.java
+++ PAGOD/src/pagod/wizard/control/states/PreConditionCheckerState.java
@@ -1,12 +1,14 @@
 /*
  * Projet PAGOD
  * 
- * $Id: PreConditionCheckerState.java,v 1.1 2005/11/06 14:30:37 fabfoot Exp $
+ * $Id: PreConditionCheckerState.java,v 1.2 2005/11/06 14:55:46 fabfoot Exp $
  */
 package pagod.wizard.control.states;
 
 import pagod.common.model.Activity;
+import pagod.utils.ActionManager;
 import pagod.wizard.control.ActivityScheduler;
+import pagod.wizard.control.Constants;
 
 /**
  * @author fabfoot
@@ -23,20 +25,34 @@ public class PreConditionCheckerState extends ActivityState
     {
         super(activityScheduler, activity);
         // TODO Corps de constructeur généré automatiquement
+        System.out.println("precond");
+        // on affiche le bouton next
+        
+        ActionManager.getInstance().getAction(Constants.ACTION_NEXT)
+                .setEnabled(true);
+
+        // on affiche le bouton terminate
+        ActionManager.getInstance().getAction(Constants.ACTION_TERMINATE)
+                .setEnabled(true);
+
+        // on affiche le bouton previous
+        ActionManager.getInstance().getAction(Constants.ACTION_PREVIOUS)
+                .setEnabled(false);
     }
 
     @Override
     public void previous()
     {
         // TODO Corps de méthode généré automatiquement
-        
+     
     }
 
     @Override
     public void next()
     {
         // TODO Corps de méthode généré automatiquement
-        
+        this.activityScheduler.setActivityState(new ActivityPresentationState(
+                this.activityScheduler, this.activity));
     }
 
     @Override
