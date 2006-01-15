c3fdb3e0c08ca390800683268cfceb6ef2d473a2
biniou <biniou>
2006-01-15 09:20:18 +0000

    Squelette de TimeActivityDialog => reste a initialiser la JTable avec son model, et implémenter les listener/editor qui vont avec Mise a jour des fichiers de langue pour l'itemmenu des temps passés et pour TimeActivityDialog

--- PAGOD/src/pagod/wizard/control/actions/TimeActivityAction.java
+++ PAGOD/src/pagod/wizard/control/actions/TimeActivityAction.java
@@ -1,19 +1,19 @@
 /*
  * Projet PAGOD
  * 
- * $Id: TimeActivityAction.java,v 1.1 2006/01/13 14:28:43 biniou Exp $
+ * $Id: TimeActivityAction.java,v 1.2 2006/01/15 09:20:18 biniou Exp $
  */
 package pagod.wizard.control.actions;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
-import pagod.common.ui.AboutDialog;
 import pagod.utils.ImagesManager;
 import pagod.utils.LanguagesManager;
 import pagod.wizard.control.ApplicationManager;
 import pagod.wizard.control.Constants;
 import pagod.wizard.control.states.Request;
+import pagod.wizard.ui.TimeActivityDialog;
 
 /**
  * @author m1isi13
@@ -30,8 +30,12 @@ public class TimeActivityAction extends AbstractPagodAction
 			IOException, ImagesManager.NotInitializedException
 	{
 		// TODO remplacer par la bonne action (timeActivity)
-		//TODO rajouter valeurs dans le bundle
-		super("about", "AboutIcon.gif", new Request(
+		// => trouver un moyen pour créer une action sans requete
+		// cf modifier encore la machine a etat?
+		
+		//TODO changer l'icone
+		
+		super("timeActivity", "AboutIcon.gif", new Request(
 				Request.RequestType.SHOW_ABOUT));
 	}
 	
@@ -45,10 +49,8 @@ public class TimeActivityAction extends AbstractPagodAction
 	{
 		// TODO enlever les commentaires quand on aura code
 		// la fenetre
-		/*AboutDialog ad = new TimeActivityDialog(ApplicationManager.getInstance().getMfPagod(),
-				Constants.APPLICATION_SHORT_NAME + " "
-						+ Constants.APPLICATION_VERSION);
-		ad.setVisible(true);*/
+		TimeActivityDialog tad = new TimeActivityDialog(ApplicationManager.getInstance().getMfPagod());
+		tad.setVisible(true);
 	}
 
 }
--- PAGOD/src/pagod/wizard/ui/TimeActivityDialog.java
+++ PAGOD/src/pagod/wizard/ui/TimeActivityDialog.java
@@ -1,24 +1,125 @@
 /*
  * Projet PAGOD
  * 
- * $Id: TimeActivityDialog.java,v 1.1 2006/01/13 14:28:42 biniou Exp $
+ * $Id: TimeActivityDialog.java,v 1.2 2006/01/15 09:20:18 biniou Exp $
  */
 package pagod.wizard.ui;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.filechooser.FileSystemView;
+
+import pagod.utils.LanguagesManager;
+import pagod.wizard.control.PreferencesManager;
+import pagod.wizard.control.PreferencesManager.FileNotExecuteException;
+import pagod.wizard.control.PreferencesManager.InvalidExtensionException;
 
 /**
- * @author m1isi13
- *
+ * @author biniou
+ * 
  */
-public class TimeActivityDialog extends JDialog
+public class TimeActivityDialog extends JDialog implements ActionListener
 {
+
+	// panel contenant la JTable
+	private JPanel	pCentral	= null;
+
+	// panel qui contiendra les boutons du bas
+	private JPanel	pButton		= null;
+
+	// Bouton Valider
+	private JButton	pbOk		= null;
+
+	// Bouton Annuler
+	private JButton	pbCancel	= null;
+
+	// Jtable
+	private JTable	table;
+
 	/**
 	 * @param parentFrame
 	 */
-	public TimeActivityDialog(JFrame parentFrame)
-    {
-        super(parentFrame);
-    }
+	public TimeActivityDialog (JFrame parentFrame)
+	{
+		super(parentFrame);
+
+		// boite de dialogue modale
+		this.setModal(true);
+
+		// titre
+		this.setTitle(LanguagesManager.getInstance().getString(
+				"TimeActivityDialogTitle"));
+
+		// creation de la JTable
+		this.table = new JTable();
+
+		// panel qui va contenir la JTable
+		// TODO remplir la JTable, définir le model ...
+		this.pCentral = new JPanel();
+		this.pCentral.add(this.table);
+
+		// panel contenant les boutons
+		this.pButton = new JPanel();
+
+		// creation des boutons Ok et annuler
+		this.pbOk = new JButton(LanguagesManager.getInstance().getString(
+				"OKButtonLabel"));
+		this.pbCancel = new JButton(LanguagesManager.getInstance().getString(
+				"CancelButtonLabel"));
+
+		// ajout des boutons au panel
+		this.pButton.add(this.pbOk);
+		this.pButton.add(this.pbCancel);
+
+		// association des listeners aux boutons
+		this.pbOk.addActionListener(this);
+		this.pbCancel.addActionListener(this);
+
+		// positionnement des panel dans la JDialog
+		this.setLayout(new BorderLayout());
+		this.getContentPane().add(this.pCentral, BorderLayout.CENTER);
+		this.getContentPane().add(this.pButton, BorderLayout.SOUTH);
+
+		this.pack();
+
+		// boîte de dialogue centrée par rapport à l'appelant
+		this.setLocationRelativeTo(parentFrame);
+	}
+
+	/**
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+
+	public void actionPerformed (ActionEvent e)
+	{
+		// on recupere le bouton sur lequel on a clique grace a
+		// l'ActionEvent e
+		JButton b = (JButton) e.getSource();
+
+		// si on a cliqué sur le bouton ok
+		if (b == TimeActivityDialog.this.pbOk)
+		{
+			// on a cliqué sur ok
+			// TODO mettre a jour le .xml avec les nouvelles valeurs
+			TimeActivityDialog.this.dispose();
+		}
+		else
+		{
+			// bouton annuler
+			TimeActivityDialog.this.dispose();
+		}
+
+	}
 }
