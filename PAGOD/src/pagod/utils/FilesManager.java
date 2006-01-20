a9e801516fc1f9e93fdc241ff94c14e732fd4897
flotueur <flotueur>
2006-01-20 15:53:49 +0000

    Ajout des fonction de copie de fichier et de repertoire ainsi que d'une fonction de suppression de repertoire

--- PAGOD/src/pagod/utils/FilesManager.java
+++ PAGOD/src/pagod/utils/FilesManager.java
@@ -1,5 +1,5 @@
 /*
- * $Id: FilesManager.java,v 1.1 2005/10/30 10:44:59 yak Exp $
+ * $Id: FilesManager.java,v 1.2 2006/01/20 15:53:49 flotueur Exp $
  *
  * PAGOD- Personal assistant for group of development
  * Copyright (C) 2004-2005 IUP ISI - Universite Paul Sabatier
@@ -35,26 +35,30 @@ import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLDecoder;
 
+import pagod.wizard.control.PreferencesManager;
+
+
 /**
- * Gestionnaire de fichier implémenté comme un singleton Cette Classe permet
- * d'accéder a des resource exterieure et permet de situer l'application.
+ * Gestionnaire de fichier impl?ment? comme un singleton Cette Classe permet
+ * d'acc?der a des resource exterieure et permet de situer l'application.
  * 
  * @author MoOky
  */
 public class FilesManager
 {
-    /**
+    
+	/**
      * Instance du gestionnaire d'images
      */
     private static FilesManager fmInstance = null;
 
     /**
-     * Chemin du repertoire ou est situé l'application
+     * Chemin du repertoire ou est situ? l'application
      */
     private String applicationPath = null;
 
     /**
-     * Constructeur privé du gestionnaire d'images (implémentation d'un
+     * Constructeur priv? du gestionnaire d'images (impl?mentation d'un
      * singleton)
      */
     private FilesManager()
@@ -88,7 +92,7 @@ public class FilesManager
             // on recupere la location du code source
             URL path = this.getClass().getProtectionDomain().getCodeSource()
                     .getLocation();
-            // on créer le fichier
+            // on cr?er le fichier
             File f;
             try
             {
@@ -113,7 +117,7 @@ public class FilesManager
             }
             catch (UnsupportedEncodingException e)
             {
-                // TODO Bloc de traitement des exceptions généré automatiquement
+                // TODO Bloc de traitement des exceptions g?n?r? automatiquement
                 ExceptionManager.getInstance().manage(e);
             }
         }
@@ -139,10 +143,10 @@ public class FilesManager
     }
 
     /**
-     * Crée un fichier temporaire à partir d'un flux
+     * Cr?e un fichier temporaire ? partir d'un flux
      * 
      * @param streamToCopy
-     *            flux a partir du quel on souhaite créer une image temporaire
+     *            flux a partir du quel on souhaite cr?er une image temporaire
      * @param fileName
      *            nom du fichier temporaire
      * @param extension
@@ -150,18 +154,18 @@ public class FilesManager
      * @param readyOnly
      *            vrai si l'on souahite que le fichier temporaire soit en
      *            lecture seule
-     * @return Fichier temporaire, null s'il n'a pu être créé
+     * @return Fichier temporaire, null s'il n'a pu ?tre cr??
      */
     public File createTempFile(InputStream streamToCopy, String fileName,
                                String extension, boolean readyOnly)
     {
         try
         {
-            // création du fichier temporaire
+            // cr?ation du fichier temporaire
             final File resultFile = File.createTempFile(fileName, extension);
             resultFile.deleteOnExit();
 
-            // création du flux de sortie
+            // cr?ation du flux de sortie
             final FileOutputStream outputStream = new FileOutputStream(
                     resultFile);
 
@@ -182,15 +186,15 @@ public class FilesManager
             //TODO a voir
             ExceptionManager.getInstance().manage(e);
         }
-        // la génération du fichier temporaire a échoué
+        // la g?n?ration du fichier temporaire a ?chou?
         return null;
     }
 
     /**
-     * Crée un fichier temporaire à partir d'un fichier
+     * Cr?e un fichier temporaire ? partir d'un fichier
      * 
      * @param fileToCopy
-     *            fichier a partir du quel on souhaite créer une image
+     *            fichier a partir du quel on souhaite cr?er une image
      *            temporaire
      * @param fileName
      *            nom du fichier temporaire
@@ -199,7 +203,7 @@ public class FilesManager
      * @param readyOnly
      *            vrai si l'on souahite que le fichier temporaire soit en
      *            lecture seule
-     * @return Fichier temporaire, null s'il n'a pu être créé
+     * @return Fichier temporaire, null s'il n'a pu ?tre cr??
      */
     public File createTempFile(File fileToCopy, String fileName,
                                String extension, boolean readyOnly)
@@ -216,4 +220,106 @@ public class FilesManager
             return null;
         }
     }
+    
+    //modif Flotueur
+    
+	/**
+	 * Copie un fichier source vers un fichier destination
+	 * 
+	 * @param source
+	 * 			
+	 * @param destination
+	 * @return boolean
+	 */
+    public static boolean copier( File source, File destination )
+    {
+            boolean resultat = false;
+            // Declaration des flux
+            java.io.FileInputStream sourceFile=null;
+            java.io.FileOutputStream destinationFile=null;
+            try {
+                    // Cr?ation du fichier :
+                    destination.createNewFile();
+                    // Ouverture des flux
+                    sourceFile = new java.io.FileInputStream(source);
+                    destinationFile = new java.io.FileOutputStream(destination);
+                    // Lecture par segment de 0.5Mo 
+                    byte buffer[]=new byte[512*1024];
+                    int nbLecture;
+                    while( (nbLecture = sourceFile.read(buffer)) != -1 ) {
+                            destinationFile.write(buffer, 0, nbLecture);
+                    } 
+                    // Copie r?ussie
+                    resultat = true;
+            } catch( java.io.FileNotFoundException f ) {
+            } catch( java.io.IOException e ) {
+            } finally {
+                    // Quoi qu'il arrive, on ferme les flux
+                    try {
+                            sourceFile.close();
+                    } catch(Exception e) { }
+                    try {
+                            destinationFile.close();
+                    } catch(Exception e) { }
+            } 
+            return( resultat );
+    }
+    
+	/**
+	 * Parcours recursivement un repertoire donne et deplace les fichiers 
+	 * vers un repertoire destination
+	 * 
+	 * @param source_directory
+	 * @param target_directory
+	 */
+    public static void copyDirectory ( File source_directory, File target_directory ) {
+        if ( source_directory.isDirectory ( ) ) {
+            File[] list = source_directory.listFiles();
+            for ( int i = 0; i < list.length; i++) {
+            		// Puisque le fichier point est un repertoire, on cree un nouveau repertoire
+            		// portant le meme nom dans le repertoire destination
+	        		File targetDir = new File(target_directory.getAbsolutePath()+list[i].getName());
+	        		if (targetDir.mkdir())
+	        		{
+	        			System.out.println("Le repertoire projet est bien cr??.");
+	        		}
+	        		else
+	        		{
+	        			System.err
+	        					.println("Le repertoire que vous voulez creer existe d?j?.");
+	        		}
+                    // Appel r?cursif sur les sous-r?pertoires
+            		copyDirectory(list[i],target_directory);
+            } 
+        } else {
+        	//Si le chemin pointe sur un fichier, on le copie dans l'emplacement destination
+        	copier(source_directory,target_directory);
+        	
+        }
+    }
+    
+    /**
+     * 
+     * @param path
+     * @return resultat
+     */
+    static public boolean deleteDirectory(File path) { 
+        boolean resultat = true; 
+        if( path.exists() ) { 
+                File[] files = path.listFiles(); 
+                for(int i=0; i<files.length; i++) { 
+                        if(files[i].isDirectory()) { 
+                                resultat &= deleteDirectory(files[i]); 
+                        } 
+                        else { 
+                        resultat &= files[i].delete(); 
+                        } 
+                } 
+        } 
+        resultat &= path.delete(); 
+        return( resultat ); 
+    }
+    
+    
+    //fin modif Flotueur
 }
