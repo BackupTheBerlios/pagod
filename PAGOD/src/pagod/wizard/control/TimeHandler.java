ae79fa2ff484e0f3977cf2b414e50f6c2b058476
garwind111 <garwind111>
2006-01-20 13:21:32 +0000

    EDcriture des methodes fillModel et loadModel

--- PAGOD/src/pagod/wizard/control/TimeHandler.java
+++ PAGOD/src/pagod/wizard/control/TimeHandler.java
@@ -1,12 +1,14 @@
 /*
  * Projet PAGOD
  * 
- * $Id: TimeHandler.java,v 1.4 2006/01/20 12:20:24 fabfoot Exp $
+ * $Id: TimeHandler.java,v 1.5 2006/01/20 13:21:32 garwind111 Exp $
  */
 package pagod.wizard.control;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Collection;
+import java.util.List;
+import java.util.Iterator;
 
 import org.jdom.Attribute;
 import org.jdom.Document;
@@ -17,6 +19,7 @@ import org.jdom.output.XMLOutputter;
 
 import pagod.common.model.Activity;
 import pagod.common.model.Process;
+
 /**
  * @author Fabfoot
  *
@@ -84,16 +87,55 @@ public TimeHandler()
 	 */
 	public void fillModel(Process process)
 	{
+		// Recuperation de la structure
+		Element rootnode = doc.getRootElement();
+		List nodelist = rootnode.getChildren("activty");
+		// Iterateur de la structure
+		Iterator nodeiterator = nodelist.iterator();
+		// Charge les activités dans une collection
+		Collection<Activity > cactivity = process.getAllActivities ();
 		
-		
+		while(nodeiterator.hasNext()) {
+			Element node = (Element) nodeiterator.next();
+			Attribute idnode = node.getAttribute("idref");
+			// Recherche l'activité dans les process
+			for (Activity acty : cactivity) {
+				String activity_id = acty.getId();
+				// Si id activité trouvé
+				if (activity_id == idnode.toString()) {
+					Element time = node.getChild("time");
+					time.setText(Integer.toString(acty.getTime()));
+					break;
+				}
+			}
+		}
 	}
 	
 	/**
 	 * charge le doc a partir du modele metier
-	 * @param process 
+	 * @param process
+	 * @return Document xml
 	 */
-	public void loadModel(Process process)
-	{}
+	public Document loadModel(Process process)
+	{
+		final Element racine = new Element("stats");
+		final Document document = new Document(racine);
+        // r?cup?ration des activit?s via le processus
+        final Collection<Activity > cactivity = process.getAllActivities ();
+        for (Activity acty : cactivity)
+        {
+        	 Element activi= new Element("activity");
+             racine.addContent(activi);
+             Attribute id = new Attribute("idref",acty.getId());
+             activi.setAttribute(id);
+             Element time = new Element ("time");
+             time.setText(Integer.toString(acty.getTime())); // Cast Int - Integer -> string
+             activi.addContent(time);
+        }
+            
+        return document;
+		
+	}
 	
 	
 	
