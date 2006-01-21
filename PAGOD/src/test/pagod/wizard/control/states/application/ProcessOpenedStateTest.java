110337d8d08ec096efa618b32f8f93d3c29db029
cyberal82 <cyberal82>
2006-01-21 17:08:58 +0000

    Ajout du test de la classe ProcessOpenedState

---
+++ PAGOD/src/test/pagod/wizard/control/states/application/ProcessOpenedStateTest.java
@@ -0,0 +1,89 @@
+/*
+ * Projet PAGOD
+ * 
+ * $Id: ProcessOpenedStateTest.java,v 1.1 2006/01/21 17:08:58 cyberal82 Exp $
+ */
+package test.pagod.wizard.control.states.application;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import pagod.common.model.Activity;
+import pagod.common.model.Product;
+import pagod.common.model.Role;
+import pagod.common.model.Step;
+import pagod.common.model.WorkDefinition;
+import pagod.wizard.control.ApplicationManager;
+import pagod.wizard.control.states.Request;
+import pagod.wizard.control.states.application.ActivityLaunchedState;
+import pagod.wizard.control.states.application.InitState;
+import pagod.wizard.control.states.application.ProcessOpenedState;
+
+/**
+ * Classe permettant de tester la classe ProcessOpenedState.
+ * 
+ * @author Alexandre Bes
+ */
+public class ProcessOpenedStateTest extends TestCase
+{
+	/**
+	 * l'objet a tester
+	 */
+	private ProcessOpenedState	processOpenedState;
+
+	/**
+	 * Constructeur de la classe de test
+	 * 
+	 */
+	public ProcessOpenedStateTest ()
+	{
+		this.processOpenedState = new ProcessOpenedState(ApplicationManager
+				.getInstance());
+	}
+
+	/**
+	 * Methode permettant de tester la methode manageRequest
+	 * 
+	 */
+	public void testManageRequest ()
+	{
+		/************* GESTION D'UNE REQUETE CLOSE_PROJECT *************/ 
+		
+		// Creation d'une requete CLOSE_PROJECT
+		Request requestCloseProject = new Request(
+				Request.RequestType.CLOSE_PROJECT);
+
+		assertTrue("L'etat doit avoir change", this.processOpenedState
+				.manageRequest(requestCloseProject));
+		assertTrue(
+				"L'etat de l'applicationManager doit etre de type InitState",
+				ApplicationManager.getInstance().getState() instanceof InitState);
+
+		
+		/************* GESTION D'UNE REQUETE RUN_ACTIVITY *************/
+		// on remet l'applicationManager dans l'etat ProcessOpenedState
+		ApplicationManager.getInstance().setState(this.processOpenedState);
+		assertTrue(ApplicationManager.getInstance().getState() instanceof ProcessOpenedState);
+		
+		Request requestRunActivity = new Request(
+				Request.RequestType.RUN_ACTIVITY);
+		
+		// creation d'une activite
+		Activity activity = new Activity("", "", null, null, new ArrayList<Step>(),
+				new WorkDefinition("", "", null, null,
+						new ArrayList<Activity>()), new ArrayList<Product>(),
+				new ArrayList<Product>(), new Role("", "", null, null,
+						new ArrayList<Activity>()));
+		
+		// on passe en parametre de la requete l'activite que l'on veut lancer
+		requestRunActivity.setContent(activity);
+
+		assertTrue("L'etat doit avoir change", this.processOpenedState
+				.manageRequest(requestRunActivity));
+		assertTrue(
+				"L'etat de l'applicationManager doit etre de type ActivityLaunchedState",
+				ApplicationManager.getInstance().getState() instanceof ActivityLaunchedState);
+		
+	}
+
+}
