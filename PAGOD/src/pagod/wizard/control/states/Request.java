cee274b949f3484e7c2c913c50b9aeab848db032
cyberal82 <cyberal82>
2006-01-30 14:46:18 +0000

    Correction du probleme des espaces qui n'apparaissent pas tjs dans la combo alors qu'ils apparaissent dans le titre de l'etape.

--- PAGOD/src/pagod/wizard/control/states/Request.java
+++ PAGOD/src/pagod/wizard/control/states/Request.java
@@ -1,7 +1,7 @@
 /*
  * Projet PAGOD
  * 
- * $Id: Request.java,v 1.6 2006/01/16 15:32:59 cyberal82 Exp $
+ * $Id: Request.java,v 1.7 2006/01/30 14:46:18 cyberal82 Exp $
  */
 package pagod.wizard.control.states;
 
@@ -167,21 +167,31 @@ public class Request
 		{
 			String sTemp = this.content.toString();
 
+			// TODO alex a suppr
+			if (sTemp.contains("grandes lignes"))
+			{
+				System.err.println("Request.toString() ");
+			}
+
 			// on remplace les &eacute; par le caractere unicode correspondant
-			// je met pas le caractere directement car le serveur marine ne les 
+			// je met pas le caractere directement car le serveur marine ne les
 			// supporte pas, il les remplace par des ?
 			sTemp = sTemp.replaceAll("&eacute;", "\u00E9");
 
-			// on remplace les &egrave; par les caracteres unicode correspondants
+			// on remplace les &egrave; par les caracteres unicode
+			// correspondants
 			sTemp = sTemp.replaceAll("&egrave;", "\u00E8");
 
-			// on remplace les &ccedil; par les caracteres unicode correspondants
+			// on remplace les &ccedil; par les caracteres unicode
+			// correspondants
 			sTemp = sTemp.replaceAll("&ccedil;", "\u00E7");
 
-			// on remplace les &ugrave; par les caracteres unicode correspondants
+			// on remplace les &ugrave; par les caracteres unicode
+			// correspondants
 			sTemp = sTemp.replaceAll("&ugrave;", "\u00F9");
 
-			// on remplace les &agrave; par les caracteres unicode correspondants
+			// on remplace les &agrave; par les caracteres unicode
+			// correspondants
 			sTemp = sTemp.replaceAll("&agrave;", "\u00E0");
 
 			// on remplace les &ecirc; par les caracteres unicode correspondants
@@ -191,11 +201,25 @@ public class Request
 			sTemp = sTemp.replaceAll("&ocirc;", "\u00F4");
 
 			// on remplace les &nbsp; par des espaces
-			sTemp = sTemp.replaceAll("&nbsp;", " ");
+			sTemp = sTemp.replaceAll("&nbsp;", "\u00A0");
+
+			// Rq : je fais dans cette ordre pour gerer de maniere simple le
+			// fait qu'un \r peut etre tt seul sur la ligne idem pour \n
+
+			// on remplace les retours chariots \r suivit d'un saut de ligne \n
+			// (ou l'inverse) par des espaces
+			sTemp = sTemp.replaceAll("\r\n", "\u00A0");
+			sTemp = sTemp.replaceAll("\n\r", "\u00A0");
+
+			// on remplace les retours chariots \r par des espaces
+			sTemp = sTemp.replaceAll("\r", "\u00A0");
+
+			// on remplace les sauts de ligne \n par des espaces
+			sTemp = sTemp.replaceAll("\n", "\u00A0");
 
 			// on supprime les espaces avant est apres la chaine
 			sTemp = sTemp.trim();
-			
+
 			return sTemp;
 		}
 		else
