266238c7c99efecab907f79c6871883bb33e07bd
yak <yak>
2006-01-31 21:34:31 +0000

    rajout des deux méthodes et du pattern observable

--- PAGOD/src/pagod/utils/TimerManager.java
+++ PAGOD/src/pagod/utils/TimerManager.java
@@ -1,46 +1,43 @@
 /*
  * Projet PAGOD
  * 
- * $Id: TimerManager.java,v 1.5 2006/01/26 19:39:52 yak Exp $
+ * $Id: TimerManager.java,v 1.6 2006/01/31 21:34:31 yak Exp $
  */
 package pagod.utils;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Observable;
 
-import javax.swing.JEditorPane;
-import javax.swing.JLabel;
 import javax.swing.Timer;
 
 /**
  * @author yak
- *
+ * 
  */
-public class TimerManager implements ActionListener
+public class TimerManager extends Observable implements ActionListener
 {
 
-	private JEditorPane jToUpdate = null;
-	private String prefix = "Temps sur l'activité :";
-	private String message ="";
 	/**
 	 * Instance du timer
 	 */
-	private static TimerManager instance = null;
+	private static TimerManager	instance		= null;
 	/**
 	 * le timer swing
 	 */
-	private static Timer theSwingTimer =  null;
-	
+	private static Timer		theSwingTimer	= null;
+
 	/**
 	 * la valeur du timer
 	 */
-	private int value;
-	
+	private int					value;
+
 	/**
 	 * Constructeur vide
 	 */
 	public TimerManager ()
 	{
-		
+
 	}
 
 	/**
@@ -51,53 +48,60 @@ public class TimerManager implements ActionListener
 		if (instance == null)
 		{
 			instance = new TimerManager();
-			theSwingTimer = new Timer(1000,instance);
+			theSwingTimer = new Timer(1000, instance);
 		}
 		return instance;
 	}
-	
+
 	/**
 	 * Demarre le timer et l'initialise si besoin est
 	 */
-	public void start()
+	public void start ()
 	{
-		//on initialise le timer a 0
-		
-			this.value = 0;
-			
-	
+		// on initialise le timer a 0
+		this.value = 0;
 		theSwingTimer.start();
+		// on notify
+		this.setChanged();
+		this.notifyObservers(new Integer(this.value));
 	}
+
 	/**
 	 * @param initValue
 	 */
 	public void start (int initValue)
 	{
-		this.jToUpdate.setText("<center>"+this.message+"<br>"+this.prefix+this.value+"</center");
 		this.value = initValue;
 		theSwingTimer.start();
-		
+		// on notify
+		this.setChanged();
+		this.notifyObservers(new Integer(this.value));
+
 	}
 
 	/**
 	 * Arrete le timer en retournant la valeur
+	 * 
 	 * @return la valeur du timer
 	 */
 	public int stop ()
 	{
-		//on arrete le timer et on retourne la valeur
+		// on arrete le timer et on retourne la valeur
 		theSwingTimer.stop();
-		
+
 		return this.value;
 	}
+
 	/**
 	 * retourne vrai si le timer est lancer
+	 * 
 	 * @return retourne vrai si le timer est lancer vrai sinon
 	 */
 	public boolean isStarted ()
 	{
 		return theSwingTimer.isRunning();
 	}
+
 	/**
 	 * 
 	 * @return la valeur du timer
@@ -107,38 +111,51 @@ public class TimerManager implements ActionListener
 	{
 		return this.value;
 	}
-	
-	
-	/** (non-Javadoc)
+
+	/**
+	 * (non-Javadoc)
+	 * 
 	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
 	 */
 	public void actionPerformed (ActionEvent arg0)
 	{
-		//on augmente la valeur
+		// on augmente la valeur
 		this.value++;
-		//mise a jour de la valeur
-		this.jToUpdate.setText("<center>"+this.message+"<br>"+this.prefix+this.value+"</center");
-		
-	}
-	
-	
-	/**
-	 * @param toUpdate Valeur à donner à jToUpdate
-	 */
-	public void setJToUpdate (JEditorPane toUpdate)
-	{
-		this.jToUpdate = toUpdate;
-	}
+		// on notifie les observer
+		this.setChanged();
+		this.notifyObservers(new Integer(this.value));
 
-	/**
-	 * @param message Valeur à donner à message
-	 */
-	public void setMessage (String message)
-	{
-		this.message = message;
 	}
-	
-	
-	
+	/**
+	 * methode permettant de transformer un int representant un temps en seocnde sous la forme stirng
+	 * @param timeInCar le string sous forme h:m:s
+	 * @return le temps sous forme de int en seconde
+	 */
+	public static int timeFromString (String timeInCar)
+	{
+
+		String tabTime[] = timeInCar.split(":");
+		int h = (Integer.parseInt(tabTime[0]));
+		int m = (Integer.parseInt(tabTime[1]));
+		int s = (Integer.parseInt(tabTime[2]));
+		return h * 3600 + m * 60 + s;
+
+	}
+	/**
+	 * methode permettant de transformer un int representant un temps en seocnde sous la forme string
+	 * @param timeInSec le temps en seconde a analyser
+	 * @return le temps sous forme h:m:s
+	 */
+	public static String stringFromTime (int timeInSec)
+	{
+		int sec = timeInSec % 60;
+		int reste = timeInSec / 60;
+
+		int minute = reste % 60;
+		int heure = reste / 60;
+		
+		return new String(heure + ":" + minute + ":" + sec);
+
+	}
 
 }
