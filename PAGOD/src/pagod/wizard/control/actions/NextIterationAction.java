a211f205583a9bc592c6e7437c5d065273c9a7f8
fabfoot <fabfoot>
2006-02-09 18:09:18 +0000

    ajout de l'action sur le next iteartion

--- PAGOD/src/pagod/wizard/control/actions/NextIterationAction.java
+++ PAGOD/src/pagod/wizard/control/actions/NextIterationAction.java
@@ -1,15 +1,19 @@
 /*
  * Projet PAGOD
  * 
- * $Id: NextIterationAction.java,v 1.1 2006/02/06 16:19:12 biniou Exp $
+ * $Id: NextIterationAction.java,v 1.2 2006/02/09 18:09:18 fabfoot Exp $
  */
 package pagod.wizard.control.actions;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.Collection;
 
+import pagod.common.model.Activity;
+import pagod.common.model.TimeCouple;
 import pagod.utils.ImagesManager;
 import pagod.utils.LanguagesManager;
+import pagod.wizard.control.ApplicationManager;
 
 /**
  * @author biniou
@@ -39,6 +43,15 @@ public class NextIterationAction extends AbstractPagodAction
 	 */
 	public void actionPerformed (ActionEvent actionEvent)
 	{
-		System.out.println("On est sensé passer à l'itération suivante.");
+		//System.out.println("On est sensé passer à l'itération suivante.");
+		int itcour = ApplicationManager.getInstance().getCurrentProject().getItCurrent() ;
+		itcour ++;
+		TimeCouple tc = new TimeCouple (0,0);
+		ApplicationManager.getInstance().getCurrentProject().setItCurrent(itcour);   
+		Collection <Activity> cAc =	ApplicationManager.getInstance().getCurrentProcess().getAllActivities();     
+		for (Activity a : cAc)
+		{
+			a.sethmTime(itcour, tc);
+		}
 	}
 }
