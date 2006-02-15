61e63bae66ddfe3a6423df8691e8cec6f3ad7a79
fabfoot <fabfoot>
2006-02-15 17:08:43 +0000

    ajout du panneau de changement d'ite

--- PAGOD/src/pagod/wizard/control/actions/NextIterationAction.java
+++ PAGOD/src/pagod/wizard/control/actions/NextIterationAction.java
@@ -1,7 +1,7 @@
 /*
  * Projet PAGOD
  * 
- * $Id: NextIterationAction.java,v 1.4 2006/02/10 15:04:44 fabfoot Exp $
+ * $Id: NextIterationAction.java,v 1.5 2006/02/15 17:08:43 fabfoot Exp $
  */
 package pagod.wizard.control.actions;
 
@@ -9,16 +9,18 @@ import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.Collection;
 
+import javax.swing.JOptionPane;
 
 import pagod.common.model.Activity;
 import pagod.common.model.TimeCouple;
+import pagod.configurator.ui.PreferencesDialog;
 import pagod.utils.ImagesManager;
 import pagod.utils.LanguagesManager;
 import pagod.wizard.control.ApplicationManager;
 
 /**
  * @author biniou
- *
+ * 
  */
 public class NextIterationAction extends AbstractPagodAction
 {
@@ -27,15 +29,16 @@ public class NextIterationAction extends AbstractPagodAction
 	 * @throws IOException
 	 * @throws LanguagesManager.NotInitializedException
 	 */
-	public NextIterationAction () throws LanguagesManager.NotInitializedException,
-			IOException, ImagesManager.NotInitializedException
+	public NextIterationAction ()
+			throws LanguagesManager.NotInitializedException, IOException,
+			ImagesManager.NotInitializedException
 	{
-		
-		//TODO changer l'icone
-		
+
+		// TODO changer l'icone
+
 		super("nextIteration", "AboutIcon.gif", null);
 	}
-	
+
 	/**
 	 * Methode appel?e lorsque l'action est d?clench?e
 	 * 
@@ -44,22 +47,29 @@ public class NextIterationAction extends AbstractPagodAction
 	 */
 	public void actionPerformed (ActionEvent actionEvent)
 	{
-		int itcour = ApplicationManager.getInstance().getCurrentProject().getItCurrent() ;
-		//mise a zero de tous les temps remaining de chaque activite de l'etape termine
-			
-	
+		int itcour = ApplicationManager.getInstance().getCurrentProject()
+				.getItCurrent();
+		// mise a zero de tous les temps remaining de chaque activite de l'etape
+		// termine
+
 		TimeCouple tc1 = null;
-		itcour ++;
-		TimeCouple tc = new TimeCouple (0,0);
-		ApplicationManager.getInstance().getCurrentProject().setItCurrent(itcour);   
-		Collection <Activity> cAc =	ApplicationManager.getInstance().getCurrentProcess().getAllActivities();     
+		itcour++;
+		TimeCouple tc = new TimeCouple(0, 0);
+		ApplicationManager.getInstance().getCurrentProject().setItCurrent(
+				itcour);
+		Collection<Activity> cAc = ApplicationManager.getInstance()
+				.getCurrentProcess().getAllActivities();
 		for (Activity a : cAc)
 		{
 			a.sethmTime(itcour, tc);
-			//pour chaque activit mettre a 0 le temps remainig
+			// pour chaque activit mettre a 0 le temps remainig
 			tc1 = a.gethmTime(itcour - 1);
 			tc1.setTimeRemaining(0);
-			
+
 		}
+		// on affiche un message comme quoi l'iteration a changé
+		JOptionPane.showMessageDialog(null, LanguagesManager.getInstance()
+				.getString("NewIteration"), LanguagesManager.getInstance()
+				.getString("NewIterationTitle"), JOptionPane.INFORMATION_MESSAGE);
 	}
 }
