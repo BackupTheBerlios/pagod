9f6b3a8ef483c998a26dc4d98b01a48080f808a3
cyberal82 <cyberal82>
2006-03-19 18:21:47 +0000

    Correction d'un petit bug

--- PAGOD/src/pagod/wizard/control/actions/ImportProjectAction.java
+++ PAGOD/src/pagod/wizard/control/actions/ImportProjectAction.java
@@ -1,7 +1,7 @@
 /*
  * Projet PAGOD
  * 
- * $Id: ImportProjectAction.java,v 1.2 2006/03/19 14:47:56 flotueur Exp $
+ * $Id: ImportProjectAction.java,v 1.3 2006/03/19 18:21:47 cyberal82 Exp $
  */
 
 package pagod.wizard.control.actions;
@@ -21,80 +21,101 @@ import pagod.utils.ImagesManager;
 import pagod.utils.LanguagesManager;
 import pagod.wizard.control.ApplicationManager;
 import pagod.wizard.control.PreferencesManager;
-import pagod.wizard.ui.MainFrame;
-import pagod.wizard.ui.PreferencesDialog;
 
 /**
  * Action pour importer des projets d'un workspace vers un autre
  * 
  * @author Flotueur
- *
+ * 
  */
 public class ImportProjectAction extends CustomAction
 {
-    /**
-     * 
-     * @throws LanguagesManager.NotInitializedException
-     * @throws IOException
-     * @throws ImagesManager.NotInitializedException
-     */
-	public ImportProjectAction() throws LanguagesManager.NotInitializedException,
-    										IOException,
-    										ImagesManager.NotInitializedException
+	/**
+	 * 
+	 * @throws LanguagesManager.NotInitializedException
+	 * @throws IOException
+	 * @throws ImagesManager.NotInitializedException
+	 */
+	public ImportProjectAction ()
+			throws LanguagesManager.NotInitializedException, IOException,
+			ImagesManager.NotInitializedException
 	{
-    	// TODO faire l'icone appropriee
+		// TODO faire l'icone appropriee
 		super("importproject", "OpenIcon.gif");
 	}
 
 	/**
 	 * Methode appele lorsque l'action est declanchee
-	 * @param e 
-	 * 			Evenement survenue
+	 * 
+	 * @param e
+	 *            Evenement survenue
 	 * 
 	 */
 	public void actionPerformed (ActionEvent e)
 	{
-        // Recuperation du workspace
-        File currentWorkspace = new File(PreferencesManager.getInstance().getWorkspace());
-        
-        // creation et affichage de l'arborescence
-        // a partir du poste de travail
-        WorkspaceFileChooser workspaceChooser = new WorkspaceFileChooser();
-       
-        // une fois qu'on a choisi une application
-        if (workspaceChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-        {
+		// Recuperation du workspace
+		File currentWorkspace = new File(PreferencesManager.getInstance()
+				.getWorkspace());
 
-            // Recupere le path du fichier choisi
-            File selectedPath = workspaceChooser.getSelectedFile();
-            
-            // On teste si le répertoire est un projet valide
-         	if (Project.isValidProjectDirectory(selectedPath)){
-        		// On créé le répertoire projet de destination
-        		File targetdir = new File(currentWorkspace.getAbsolutePath()+File.separator+selectedPath.getName());
-        		targetdir.mkdir();
-        		// On recopie le répertoire
-        		if (FilesManager.copyDirectory(selectedPath,targetdir,targetdir) 
-        				&& !selectedPath.equals(targetdir)
-        				&& !targetdir.exists()){
-	        		// Le répertoire a bien été copié, on affiche un message à l'utilisateur
-					JOptionPane.showMessageDialog(ApplicationManager.getInstance().getMfPagod(), LanguagesManager.getInstance().getString("infoImportProject"),
-							LanguagesManager.getInstance().getString("titleInfoImportProject"),JOptionPane.INFORMATION_MESSAGE);
-        		}
-        		else{
-        			// Il y a eu une erreur durant la copie et l'import ne s'est pas fait, on avertit l'utilisateur
-					JOptionPane.showMessageDialog(ApplicationManager.getInstance().getMfPagod(), LanguagesManager.getInstance().getString("ImportProjectException"),
-							LanguagesManager.getInstance().getString("ImportProjectErrorTitle"),JOptionPane.ERROR_MESSAGE);
+		// creation et affichage de l'arborescence
+		// a partir du poste de travail
+		WorkspaceFileChooser workspaceChooser = new WorkspaceFileChooser();
 
-        		}
-             }
-         	else
-         	{
-         		// le répertoire n'est pas un projet valide, on prévient l'utilisateur
-				JOptionPane.showMessageDialog(ApplicationManager.getInstance().getMfPagod(), LanguagesManager.getInstance().getString("ImportProjectNull"),
-						LanguagesManager.getInstance().getString("ImportProjectErrorTitle"),JOptionPane.ERROR_MESSAGE);
-         	}
-        }
+		// une fois qu'on a choisi une application
+		if (workspaceChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+		{
+
+			// Recupere le path du fichier choisi
+			File selectedPath = workspaceChooser.getSelectedFile();
+
+			// On teste si le répertoire est un projet valide
+			if (Project.isValidProjectDirectory(selectedPath))
+			{
+				// On créé le répertoire projet de destination
+				File targetdir = new File(currentWorkspace.getAbsolutePath()
+						+ File.separator + selectedPath.getName());
+				
+				// TODO A SUPPR targetdir.mkdir();
+				
+				// On recopie le répertoire
+				if (targetdir.mkdir() /*&& targetdir.exists()*/
+						&& !selectedPath.equals(targetdir)
+						&& FilesManager.copyDirectory(selectedPath, targetdir,
+								targetdir))
+				{
+					// Le répertoire a bien été copié, on affiche un message à
+					// l'utilisateur
+					JOptionPane.showMessageDialog(ApplicationManager
+							.getInstance().getMfPagod(), LanguagesManager
+							.getInstance().getString("infoImportProject"),
+							LanguagesManager.getInstance().getString(
+									"titleInfoImportProject"),
+							JOptionPane.INFORMATION_MESSAGE);
+				}
+				else
+				{
+					// Il y a eu une erreur durant la copie et l'import ne s'est
+					// pas fait, on avertit l'utilisateur
+					JOptionPane.showMessageDialog(ApplicationManager
+							.getInstance().getMfPagod(), LanguagesManager
+							.getInstance().getString("ImportProjectException"),
+							LanguagesManager.getInstance().getString(
+									"ImportProjectErrorTitle"),
+							JOptionPane.ERROR_MESSAGE);
+
+				}
+			}
+			else
+			{
+				// le répertoire n'est pas un projet valide, on prévient
+				// l'utilisateur
+				JOptionPane.showMessageDialog(ApplicationManager.getInstance()
+						.getMfPagod(), LanguagesManager.getInstance()
+						.getString("ImportProjectNull"), LanguagesManager
+						.getInstance().getString("ImportProjectErrorTitle"),
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
 
 	}
 }
